dd31ac8ce6bbf09dd7dd59ecb92cc9145d1b1324
dimagong <dimagong@gmail.com>
2020-07-15 15:26:37 +0300

    fix style

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { createStore, applyMiddleware } from 'redux'
-//import createSagaMiddleware from 'redux-saga'
 import { Provider } from 'react-redux'
-//import { composeWithDevTools } from 'redux-devtools-extension'
-//import { logger } from 'redux-logger'
-
 import { store } from './store/store'
-
-//import reducer from './Redux/reducers/reduceFavorite'
-//import allReducers from './Redux/reducers'
-
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-//import middlewareRequest from './Redux/Middleware/MiddlewareRequest'
 import './index.css';
-
-
-//const sagaMiddleware = createSagaMiddleware(middlewareRequest)
-//const store = createStore(reducer)
-//const store = createStore(allReducers, composeWithDevTools(applyMiddleware(sagaMiddleware, logger),))
-//sagaMiddleware.run(middlewareRequest)
 
 ReactDOM.render(
 
